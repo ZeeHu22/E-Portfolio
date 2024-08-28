e36f658f64c97155000bcaa6a728d7db4735d4f3
Zeeshan <zeeshan.hu@outlook.com>
2024-08-28 18:25:32 -0400

    media draft commit

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+//template_d4a50yj
+//service_dphzodj
+//Z3SkQxg_ht0haJhhv
+
 let contrastToggle = false;
+let isModalOpen = false;
+const scaleFactor = 1 / 20;
+
+function moveShapes(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for(let i = 0; i < shapes.length; i++) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px`
+    }
+}
 
 function toggleContrast() {
     contrastToggle = !contrastToggle;
@@ -24,4 +42,35 @@ function copyEmailToClipboard() {
     setTimeout(function() {
         customAlert.classList.remove('show');
     }, 3000); // Hide the alert after 3 seconds
+}
+
+function contact(event){
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+    emailjs
+        .sendForm(
+            'service_dphzodj',
+            'template_d4a50yj',
+            event.target,
+            'Z3SkQxg_ht0haJhhv'
+        ).then(() => {
+            loading.classList.remove(",odal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly at zeeshan.hu@outlook.com"
+            )
+        })
+
+}
+
+function toggleModal(){
+    isModalOpen = !isModalOpen;
+    if(!isModalOpen) {
+        return document.body.classList.remove("modal--open");
+    }
+    document.body.classList += " modal--open";
 }
